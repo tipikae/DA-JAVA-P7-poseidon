6cb30f1f0185831b0abd01aa6356c2ab490100ac
tipikae <gilles.bd@free.fr>
2022-02-09 09:40:41 +0100

    adding parent exception

---
+++ src/main/java/com/nnk/springboot/exceptions/PoseidonException.java
@@ -0,0 +1,18 @@
+package com.nnk.springboot.exceptions;
+
+/**
+ * Poseidon exception.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class PoseidonException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public PoseidonException(String arg0) {
+		super(arg0);
+	}
+
+	
+}
