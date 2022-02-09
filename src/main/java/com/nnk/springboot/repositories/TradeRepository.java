ca255aed6979706492396a8a3436cf6f4ecad362
tipikae <gilles.bd@free.fr>
2022-02-09 09:17:25 +0100

    adding annotation and javadoc syntax to repositories

--- src/main/java/com/nnk/springboot/repositories/TradeRepository.java
+++ src/main/java/com/nnk/springboot/repositories/TradeRepository.java
@@ -2,7 +2,14 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+/**
+ * Trade repository interface.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+@Repository
 public interface TradeRepository extends JpaRepository<Trade, Integer> {
 }
