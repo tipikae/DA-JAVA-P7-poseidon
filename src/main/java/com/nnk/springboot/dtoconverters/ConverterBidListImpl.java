3a0d1ee0e416ddb5c97846b67483ea5e73212fbe
tipikae <gilles.bd@free.fr>
2022-02-10 15:22:19 +0100

    adding converter BidList to DTO

---
+++ src/main/java/com/nnk/springboot/dtoconverters/ConverterBidListImpl.java
@@ -0,0 +1,60 @@
+package com.nnk.springboot.dtoconverters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.dto.BidListDTO;
+import com.nnk.springboot.exceptions.ConverterException;
+
+/**
+ * BidList converter.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class ConverterBidListImpl implements IConverterBidList {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ConverterBidListImpl.class);
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public BidListDTO convertEntityToDTO(BidList bidList) throws ConverterException {
+		if(bidList.getAccount().equals("") || bidList.getType().equals("")) {
+			LOGGER.debug("convertEntityToDTO: ConverterException: account=" 
+					+ bidList.getAccount() + ", type=" + bidList.getType());
+			throw new ConverterException("Empty field");
+		}
+		BidListDTO bidListDTO = new BidListDTO();
+		bidListDTO.setAccount(bidList.getAccount());
+		bidListDTO.setBidQuantity(bidList.getBidQuantity());
+		bidListDTO.setId(bidList.getBidListId());
+		bidListDTO.setType(bidList.getType());
+		
+		return bidListDTO;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<BidListDTO> convertListEntityToDTO(List<BidList> bidLists) throws ConverterException {
+		List<BidListDTO> bidListDTOs = new ArrayList<>();
+		for(BidList bidList: bidLists) {
+			try {
+				bidListDTOs.add(convertEntityToDTO(bidList));
+			} catch (ConverterException e) {
+				LOGGER.debug("convertListEntityToDTO: ConverterException: " + e.getMessage());
+				throw new ConverterException("Empty field");
+			}
+		}
+		
+		return bidListDTOs;
+	}
+
+}
---
+++ src/main/java/com/nnk/springboot/dtoconverters/IConverterBidList.java
@@ -0,0 +1,14 @@
+package com.nnk.springboot.dtoconverters;
+
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.dto.BidListDTO;
+
+/**
+ * BidList converter interface.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public interface IConverterBidList extends IConverter<BidList, BidListDTO> {
+
+}
