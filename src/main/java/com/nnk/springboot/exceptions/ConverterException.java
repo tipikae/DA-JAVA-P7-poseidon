4ffeaa37762328367fcda61f73dfd2bc44f18e48
tipikae <gilles.bd@free.fr>
2022-02-10 15:01:11 +0100

    adding converter exception

---
+++ src/main/java/com/nnk/springboot/exceptions/ConverterException.java
@@ -0,0 +1,17 @@
+package com.nnk.springboot.exceptions;
+
+/**
+ * Converter exception.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class ConverterException extends PoseidonException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ConverterException(String arg0) {
+		super(arg0);
+	}
+
+}
