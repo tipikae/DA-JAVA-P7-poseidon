b3e2c1eff1e0da4d8233ad8de007d004b28bdf6b
tipikae <gilles.bd@free.fr>
2022-02-10 14:55:39 +0100

    updating BidList interface

---
+++ src/main/java/com/nnk/springboot/exceptions/ServiceException.java
@@ -0,0 +1,17 @@
+package com.nnk.springboot.exceptions;
+
+/**
+ * Service Exception.
+ * @author tipikae
+ * @version 1.0
+ *
+ */
+public class ServiceException extends PoseidonException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ServiceException(String arg0) {
+		super(arg0);
+	}
+
+}
--- src/main/java/com/nnk/springboot/services/IBidListService.java
+++ src/main/java/com/nnk/springboot/services/IBidListService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.nnk.springboot.dto.BidListDTO;
 import com.nnk.springboot.dto.NewBidListDTO;
+import com.nnk.springboot.exceptions.NotFoundException;
+import com.nnk.springboot.exceptions.ServiceException;
 
 /**
  * BidList Service interface.
@@ -13,9 +15,44 @@ import com.nnk.springboot.dto.NewBidListDTO;
  */
 public interface IBidListService {
 
-	BidListDTO addBidList(NewBidListDTO bid);
-	List<BidListDTO> getAllBids();
-	BidListDTO getBidList(Integer id);
-	void updateBidList(Integer id, NewBidListDTO bid);
-	void deleteBidList(Integer id);
+	/**
+	 * Add a BidList.
+	 * @param bid
+	 * @return BidListDTO
+	 * @throws ServiceException
+	 */
+	BidListDTO addBidList(NewBidListDTO bid) throws ServiceException;
+	
+	/**
+	 * Get all BidLists.
+	 * @return List<BidListDTO>
+	 * @throws ServiceException
+	 */
+	List<BidListDTO> getAllBids() throws ServiceException;
+	
+	/**
+	 * Get a BidList.
+	 * @param id
+	 * @return BidListDTO
+	 * @throws NotFoundException
+	 * @throws ServiceException
+	 */
+	BidListDTO getBidList(Integer id) throws NotFoundException, ServiceException;
+	
+	/**
+	 * Update a BidList.
+	 * @param id
+	 * @param bid
+	 * @throws NotFoundException
+	 * @throws ServiceException
+	 */
+	void updateBidList(Integer id, NewBidListDTO bid) throws NotFoundException, ServiceException;
+	
+	/**
+	 * Delete a BidList.
+	 * @param id
+	 * @throws NotFoundException
+	 * @throws ServiceException
+	 */
+	void deleteBidList(Integer id) throws NotFoundException, ServiceException;
 }
