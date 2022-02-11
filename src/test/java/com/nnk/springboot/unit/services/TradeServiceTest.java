be903c3e4a9e81fe3d609e3c13c2c0eff77781ec
tipikae <gilles.bd@free.fr>
2022-02-11 11:05:20 +0100

    adding trade service tests

---
+++ src/test/java/com/nnk/springboot/unit/services/TradeServiceTest.java
@@ -0,0 +1,14 @@
+package com.nnk.springboot.unit.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TradeServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
