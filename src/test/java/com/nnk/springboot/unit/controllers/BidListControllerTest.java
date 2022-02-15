35aba61a6745c2b6ab4004bf5f150f2d7d9e1d8a
tipikae <gilles.bd@free.fr>
2022-02-15 15:53:19 +0100

    writing bidlist controller

---
+++ src/test/java/com/nnk/springboot/unit/controllers/BidListControllerTest.java
@@ -0,0 +1,14 @@
+package com.nnk.springboot.unit.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BidListControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
