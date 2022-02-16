4b8a151695abd01453b065fb32d04f61cc8e33c3
tipikae <gilles.bd@free.fr>
2022-02-16 10:21:54 +0100

    adding ruleName controller

--- src/main/java/com/nnk/springboot/controllers/RuleNameController.java
+++ src/main/java/com/nnk/springboot/controllers/RuleNameController.java
@@ -1,54 +1,204 @@
 package com.nnk.springboot.controllers;
 
-import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.dto.NewRuleNameDTO;
+import com.nnk.springboot.dto.RuleNameDTO;
+import com.nnk.springboot.dto.UpdateRuleNameDTO;
+import com.nnk.springboot.exceptions.ConverterException;
+import com.nnk.springboot.exceptions.NotFoundException;
+import com.nnk.springboot.exceptions.ServiceException;
+import com.nnk.springboot.services.IRuleNameService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @Controller
 public class RuleNameController {
-    // TODO: Inject RuleName service
+    
+	private static final Logger LOGGER = LoggerFactory.getLogger(RuleNameController.class);
+	
+	@Autowired
+	private IRuleNameService ruleNameService;
 
+	/**
+	 * Get all ruleNames.
+	 * @param model
+	 * @return String
+	 */
     @RequestMapping("/ruleName/list")
-    public String home(Model model)
-    {
-        // TODO: find all RuleName, add to model
+    public String home(HttpServletRequest request, Model model) {
+    	LOGGER.debug("Getting all ruleNames");
+    	try {
+			List<RuleNameDTO> dtos = ruleNameService.getAllItems();
+			model.addAttribute("ruleNames", dtos);
+		} catch (ConverterException e) {
+			LOGGER.debug("Home: ConverterException: " + e.getMessage());
+			return "error/400";
+		} catch (Exception e) {
+			LOGGER.debug("Home: Exception: " + e.getMessage());
+			return "error/400";
+		}
         return "ruleName/list";
     }
 
+    /**
+     * Get add ruleName form.
+     * @param request
+     * @return String
+     */
     @GetMapping("/ruleName/add")
-    public String addRuleForm(RuleName bid) {
+    public String addRuleForm(HttpServletRequest request) {
+    	LOGGER.debug("Getting add ruleName form");
         return "ruleName/add";
     }
 
+    /**
+     * Add a ruleName.
+     * @param request
+     * @param newRuleNameDTO
+     * @param result
+     * @param model
+     * @return String
+     */
     @PostMapping("/ruleName/validate")
-    public String validate(@Valid RuleName ruleName, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return RuleName list
-        return "ruleName/add";
+    public String validate(
+    		HttpServletRequest request, 
+    		@ModelAttribute("ruleName") @Valid NewRuleNameDTO newRuleNameDTO, 
+    		BindingResult result, 
+    		Model model) {
+
+    	LOGGER.debug("Posting a ruleName");
+    	if(result.hasErrors()) {
+    		StringBuilder sb = new StringBuilder();
+    		result.getAllErrors().stream().forEach(e -> sb.append(e.getDefaultMessage() + " "));
+			LOGGER.debug("has errors:" + sb);
+			return "redirect:/ruleName/add?error=" + sb;
+    	}
+    	
+    	try {
+			RuleNameDTO ruleName = ruleNameService.addItem(newRuleNameDTO);
+			model.addAttribute("ruleName", ruleName);
+		} catch (ServiceException e) {
+			LOGGER.debug("Validate: ServiceException: " + e.getMessage());
+			return "redirect:/ruleName/add?error=Unable to process new RuleName.";
+		} catch (ConverterException e) {
+			LOGGER.debug("Validate: ConverterException: " + e.getMessage());
+			return "redirect:/ruleName/add?error=Unable to process new RuleName.";
+		} catch (Exception e) {
+			LOGGER.debug("Validate: Exception: " + e.getMessage());
+			return "redirect:/ruleName/add?error=Unable to process new RuleName.";
+		}
+    	
+        return "redirect:/ruleName/list?success=New RuleName added.";
     }
 
+    /**
+     * Get update ruleName form.
+     * @param request
+     * @param id
+     * @param model
+     * @return String
+     */
     @GetMapping("/ruleName/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get RuleName by Id and to model then show to the form
+    public String showUpdateForm(
+    		HttpServletRequest request, 
+    		@PathVariable("id") @Positive Integer id, 
+    		Model model) {
+    	
+    	LOGGER.debug("Getting a ruleName to update");
+    	
+    	try {
+			RuleNameDTO ruleName = ruleNameService.getItemById(id);
+			model.addAttribute("ruleName", ruleName);
+		} catch (NotFoundException e) {
+			LOGGER.debug("ShowUpdateForm: NotFoundException: " + e.getMessage());
+			return "error/404";
+		} catch (ConverterException e) {
+			LOGGER.debug("ShowUpdateForm: ConverterException: " + e.getMessage());
+			return "error/400";
+		} catch (Exception e) {
+			LOGGER.debug("ShowUpdateForm: Exception: " + e.getMessage());
+			return "error/400";
+		}
+    	
         return "ruleName/update";
     }
 
+    /**
+     * Update a ruleName.
+     * @param request
+     * @param id
+     * @param updateRuleNameDTO
+     * @param result
+     * @param model
+     * @return String
+     */
     @PostMapping("/ruleName/update/{id}")
-    public String updateRuleName(@PathVariable("id") Integer id, @Valid RuleName ruleName,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update RuleName and return RuleName list
-        return "redirect:/ruleName/list";
+    public String updateRuleName(
+    		HttpServletRequest request, 
+    		@PathVariable("id") @Positive Integer id, 
+    		@ModelAttribute("ruleName") @Valid UpdateRuleNameDTO updateRuleNameDTO,
+            BindingResult result, 
+            Model model) {
+    	
+    	LOGGER.debug("Updating a ruleName");
+    	if(result.hasErrors()) {
+    		StringBuilder sb = new StringBuilder();
+    		result.getAllErrors().stream().forEach(e -> sb.append(e.getDefaultMessage() + " "));
+			LOGGER.debug("has errors:" + sb);
+			return "redirect:/ruleName/update/" + id + "?error=" + sb;
+    	}
+    	
+    	try {
+    		ruleNameService.updateItem(id, updateRuleNameDTO);
+		} catch (NotFoundException e) {
+			LOGGER.debug("updateRuleName: NotFoundException: " + e.getMessage());
+			return "redirect:/ruleName/update/" + id + "?error=RuleName not found.";
+		} catch (Exception e) {
+			LOGGER.debug("updateRuleName: Exception: " + e.getMessage());
+			return "redirect:/ruleName/update/" + id + "?error=Unable to process update RuleName.";
+		}
+    	
+        return "redirect:/ruleName/list?success=RuleName has been updated.";
     }
 
+    /**
+     * Delete a ruleName.
+     * @param request
+     * @param id
+     * @param model
+     * @return String
+     */
     @GetMapping("/ruleName/delete/{id}")
-    public String deleteRuleName(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find RuleName by Id and delete the RuleName, return to Rule list
-        return "redirect:/ruleName/list";
+    public String deleteRuleName(
+    		HttpServletRequest request, 
+    		@PathVariable("id") @Positive Integer id, 
+    		Model model) {
+    	
+    	LOGGER.debug("Deleting a ruleName");
+    	try {
+    		ruleNameService.deleteItem(id);
+		} catch (NotFoundException e) {
+			LOGGER.debug("deleteRuleName: NotFoundException: " + e.getMessage());
+			return "redirect:/ruleName/delete/" + id + "?error=RuleName not found.";
+		} catch (Exception e) {
+			LOGGER.debug("deleteRuleName: Exception: " + e.getMessage());
+			return "redirect:/ruleName/delete/" + id + "?error=Unable to process delete RuleName.";
+		}
+        return "redirect:/ruleName/list?success=RuleName has been deleted.";
     }
 }
--- src/main/java/com/nnk/springboot/dto/UpdateRuleNameDTO.java
+++ src/main/java/com/nnk/springboot/dto/UpdateRuleNameDTO.java
@@ -5,6 +5,7 @@ package com.nnk.springboot.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -23,36 +24,42 @@ public class UpdateRuleNameDTO implements Serializable {
 	/**
 	 * Name.
 	 */
+	@NotBlank(message = "Name must not be empty.")
 	@Size(max=125, message="{validation.name.size.too_long}")
 	private String name;
 
 	/**
 	 * Description.
 	 */
+	@NotBlank(message = "Description must not be empty.")
 	@Size(max=125, message="{validation.name.size.too_long}")
 	private String description;
 
 	/**
 	 * Json.
 	 */
+	@NotBlank(message = "Json must not be empty.")
 	@Size(max=125, message="{validation.name.size.too_long}")
 	private String json ;
 
 	/**
 	 * Template.
 	 */
+	@NotBlank(message = "Template must not be empty.")
 	@Size(max=512, message="{validation.name.size.too_long}")
 	private String template;
 
 	/**
 	 * Sql string.
 	 */
+	@NotBlank(message = "SQL string must not be empty.")
 	@Size(max=125, message="{validation.name.size.too_long}")
 	private String sqlStr;
 
 	/**
 	 * Sql part.
 	 */
+	@NotBlank(message = "SQL part must not be empty.")
 	@Size(max=125, message="{validation.name.size.too_long}")
 	private String sqlPart;
 }
