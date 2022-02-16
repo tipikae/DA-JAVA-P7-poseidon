6a42c5f833cf1573ae6a51c4987e752d33565618
tipikae <gilles.bd@free.fr>
2022-02-16 14:51:16 +0100

    adding login controller tests

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,40 +1,55 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.repositories.UserRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("app")
 public class LoginController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("login")
+    /**
+     * Get login page.
+     * @return ModelAndView
+     */
+    @GetMapping("/login")
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
     }
 
-    @GetMapping("secure/article-details")
-    public ModelAndView getAllUserArticles() {
+    /**
+     * Get all users.
+     * @return ModelAndView
+     */
+    @GetMapping("/secure/users")
+    public ModelAndView getAllUsers() {
         ModelAndView mav = new ModelAndView();
         mav.addObject("users", userRepository.findAll());
         mav.setViewName("user/list");
         return mav;
     }
 
-    @GetMapping("error")
+    /**
+     * Get error page
+     * @return ModelAndView
+     */
+    @GetMapping("/error")
     public ModelAndView error() {
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
-        mav.setViewName("403");
+        mav.setViewName("error/403");
         return mav;
     }
 }
